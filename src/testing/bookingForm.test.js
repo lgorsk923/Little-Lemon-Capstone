589025409ffbdd880456c3adf40a352fe25993a7
lgorsk923 <118467623+lgorsk923@users.noreply.github.com>
2024-08-17 20:31:26 -0500

    Test is now functioning and passing!

---
+++ src/testing/bookingForm.test.js
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom'
+import { render, fireEvent, screen, cleanup, getAllByTestId, queryAllByTestId } from '@testing-library/react';
+import { BookingForm } from '../Pages/Booking/BookingForm';
+
+afterEach(() => cleanup());
+
+test('Renders second half of form', () => {
+    render(<BookingForm />);
+
+    const ContactHeading = screen.getByText('Complete Your Booking!');
+
+    expect(ContactHeading).toBeInTheDocument();
+});
+
+test('default value for time selection is rendering', () => {
+    render(<BookingForm />);
+
+    const timeSelection = screen.getByText('Select Time');
+
+    expect(timeSelection).toBeInTheDocument();
+});
+
+test('time options are updating depending on the date selected', () => {
+    const { rerender } = render(<BookingForm />);
+    const dateInput = screen.getByLabelText('Date');
+    const timeSelection = screen.getByText('Select Time');
+
+    fireEvent.focus(dateInput, { target: { value: '09/30/2024' } });
+
+    expect(dateInput).toHaveValue('09/30/2024');
+
+    let availability = screen.getAllByTestId('time-option');
+
+    expect(timeSelection).toHaveValue('Select Time');
+
+    rerender(<BookingForm />);
+    expect(availability).toHaveLength(15);
+});
